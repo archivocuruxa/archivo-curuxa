fe777bc435231062cb2c08af4f10d64b61c47cca
archivocuruxa <raul.corullon@gmail.com>
2026-06-27 18:19:32 +0200

    Volcado 9 primeras revistas

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("Archivo Curuxa iniciado");
+const contenedor = document.getElementById("revistas");
+
+contenedor.innerHTML = `
+<div class="tarjeta">
+
+    <img src="imagenes/N01_04-1983.jpg">
+
+    <h2>Nº 1</h2>
+
+    <p>Abril 1983</p>
+
+</div>
+`;
